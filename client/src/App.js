877287efb96cac307db695c0913f4933f408a367
Zaid <zaidsaeed>
2019-11-07 21:50:55 -0500

    half way through creating the login query

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import Navbar from './AppComponents/Navbar';
+import React from "react";
+import Navbar from "./AppComponents/Navbar";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 function App() {
   return (
-
-    <div>
-      <Navbar/>
-      <h1>Register user</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Navbar />
+        <h1>Register user</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
