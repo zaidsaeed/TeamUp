c99ea1c01ca8dec4c9f4ab4f203a27f14571a217
ahmedGhub <agawi052@uottawa.ca>
2019-11-19 16:47:04 -0500

    created a schema for teams

---
+++ server/schemas/TeamSchema.py
@@ -0,0 +1,50 @@
+# flask_sqlalchemy/schema.py
+import graphene
+from graphene import relay
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from models import db_session, User as UserModel
+from models import db_session, Course as CourseModel
+from models import db_session, Team as TeamModel
+import utils
+from datetime import datetime
+
+class TeamAttribute:
+	
+    members_count= graphene.Int(description="Current number of members")
+    teamname= graphene.String(description = "Team name")
+    idteam = graphene.Int(description="Team ID")
+    idcourse= graphene.String(description = "Team Course ID")
+    idprof = graphene.Int(description = "Team instructor ID")
+    created_at = graphene.types.datetime.Date(description = "Team creation Time")
+    
+
+
+class Team(SQLAlchemyObjectType, TeamAttribute):
+    class Meta:
+        model = TeamModel
+        interfaces = (relay.Node, )
+
+class TeamConnection(relay.Connection):
+    class Meta:
+        node = Team
+
+class CreateTeamInput(graphene.InputObjectType, TeamAttribute):
+    """Arguments to create a Team."""
+    pass
+
+class CreateTeam(graphene.Mutation):
+    """Mutation to create a Team."""
+    team = graphene.Field(lambda: Team, description="Team created by this mutation.")
+
+    class Arguments:
+        input = CreateTeamInput(required=True)
+
+    def mutate(self, info, input):
+        data = utils.input_to_dictionary(input)
+        # data['created'] = datetime.utcnow()
+        # data['edited'] = datetime.utcnow()
+
+        Team = TeamModel(**data)
+        db_session.add(team)
+        db_session.commit()
+        return CreateTeam(team=team)
