8918123535c1a231fb65083550c24e6d648c594e
ahmedGhub <agawi052@uottawa.ca>
2019-12-01 04:26:51 -0500

    added some commented out code

--- client/src/AppComponents/JoinRequest.js
+++ client/src/AppComponents/JoinRequest.js
@@ -6,6 +6,42 @@ import { connect } from "react-redux";
 import { compose } from "recompose";
 import classnames from "classnames";
 
+
+// const STUDENT= window.localStorage.setItem("user", JSON.stringify(data.data));
+
+
+
+// const JOIN_REQUEST = gql`
+//   mutation joinRequest(
+//     $idteam:Int!
+//     $idprof: Int!
+//     $idcourse:String!
+//     $idstudent: Int!
+    
+//   ) {
+//     joinRequest(
+//       input: {
+//         idteam: $idteam
+//         idcourse: $idcourse
+//         idprof: $idprof
+//         idstudent: ?????
+        
+        
+//         }
+//     ) {
+//       team??? {
+//         idliason
+//         membersCount
+//         teamname
+//         idteam
+//         idcourse
+//         idprof
+//         createdAt
+//       }
+//     }
+//   }
+// `;
+
 const VIEWTEAMS_QUERY = gql`
   query{
   allTeams {
@@ -27,20 +63,30 @@ const VIEWTEAMS_QUERY = gql`
 class JoinRequest extends Component {
   constructor(props) {
     super(props);
+    this.state={
+        selectedTeam : null
+    };
+    
+  }
+
+  onTeamSelect(team) {
+    //   console.log(STUDENT);
+    this.setState({ selectedTeam: team});
     
   }
 
   renderTeam(team) {
+      const currentteam = team.node;
     if (team != null)
       return (
         
-          <div className="card text-white bg-success mb-3" >
-            <div className="card-header" >Team ID: {team.node.idteam}</div>
+          <div className="card text-white bg-success mb-3"  >
+            <div className="card-header" >Team ID: {currentteam.idteam}</div>
             <div className="card-body">
-              <button type="submit" class="btn btn-secondary my-2 my-sm-0" style={{float:'right'}}>Request to join team</button>
-              <h3 className="card-title">{team.node.teamname}</h3>
-              <h6 className="card-title">{team.node.idcourse}</h6>
-              <p className="card-text">Maximum number of students is: {team.node.membersCount}</p>
+              <button type="submit" className="btn btn-secondary my-2 my-sm-0" style={{float:'right'}} value={currentteam} onClick={() => this.onTeamSelect(currentteam)}>Request to join team</button>
+              <h3 className="card-title">{currentteam.teamname}</h3>
+              <h6 className="card-title">{currentteam.idcourse}</h6>
+              <p className="card-text">Maximum number of students is: {currentteam.membersCount}</p>
 
             </div>
           </div>
