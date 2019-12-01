ea3d24a9582b6eaa748faa2cf4e92fc38aeda24f
ahmedGhub <agawi052@uottawa.ca>
2019-11-30 23:43:48 -0500

    only changed the render function

--- client/src/AppComponents/ViewTeams.js
+++ client/src/AppComponents/ViewTeams.js
@@ -31,19 +31,19 @@ class ViewTeams extends Component {
     // };
   }
 
-  // renderTeam(team) {
-  //   if (team != null)
-  //     return (
-  //       <Card>
-  //         <CardBody>
-  //           <CardTitle>{team.teamname}</CardTitle>
-  //           <CardText>{team.idcourse}</CardText>
-  //           <CardText>{team.memberscount}</CardText>
-  //         </CardBody>
-  //       </Card>
-  //     );
-  //   else return <div></div>;
-  // }
+  renderTeam(team) {
+    if (team != null)
+      return (
+        <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h4 class="card-title">Primary card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      );
+    else return <div></div>;
+  }
 
   render() {
     // const list = TEAMS.map(team => {
