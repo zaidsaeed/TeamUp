005f6af93a884e992378c7e4586a5cf798a3eedb
Zaid <zaidsaeed>
2020-01-07 20:46:17 -0500

    Updates

---
+++ precache-manifest.49bb36222648f58f028c9510a5057973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0196dfa4a2c7874ff7282c191afc4fe",
+    "url": "/TeamUp/index.html"
+  },
+  {
+    "revision": "d2b7bfe98269e05f293e",
+    "url": "/TeamUp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d6fbd8d1e76f4512c6cf",
+    "url": "/TeamUp/static/js/2.d89cf54f.chunk.js"
+  },
+  {
+    "revision": "d2b7bfe98269e05f293e",
+    "url": "/TeamUp/static/js/main.437eda17.chunk.js"
+  },
+  {
+    "revision": "d95197bfe8352fe474fc",
+    "url": "/TeamUp/static/js/runtime-main.fe85150a.js"
+  },
+  {
+    "revision": "74b195648f74554cf07d3aa937417376",
+    "url": "/TeamUp/static/media/background.74b19564.png"
+  }
+]);
